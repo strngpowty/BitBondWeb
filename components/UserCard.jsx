6ae160e8a1cffd213ae3895aeaa6166564ea3c99
strngpowty <strngpowty@gmail.com>
2025-06-05 22:21:07 +0530

    WIP : usercard

--- components/UserCard.jsx
+++ components/UserCard.jsx
@@ -1,30 +1,48 @@
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const UserCard = () => {
-    const feed = useSelector((store) => store.feed)
-    // const feed0 = feed[0]
-    console.log(feed)   
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  const feed = useSelector((store) => store.feed);
+  const feed0 = feed?.[0];
+  console.log(feed0);
+
+  if (!user) {
+    navigate("/login")
+  }
+
   return (
-    <>
-      <div className="card bg-base-100 w-96 shadow-sm">
-        <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Card Title</h2>
-          <p>
-            A card component has a figure, a body part, and inside body there
-            are title and actions parts
-          </p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+    <div className="flex-grow flex justify-center items-center pb-20">
+      <div className="flex flex-col md:flex-row items-center gap-4">
+        {/* Pass Button */}
+        <button className="btn btn-default w-full md:w-auto">Pass</button>
+
+        {/* Card */}
+        <div className="card bg-base-300 w-80 shadow-sm">
+          <figure>
+            <img
+              src={feed0?.photoURL}
+              alt="User Image"
+              className="object-cover w-full h-64"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Card Title</h2>
+            <p>
+              A card component has a figure, a body part, and inside body there
+              are title and actions parts
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
           </div>
         </div>
+
+        {/* Like Button */}
+        <button className="btn btn-default w-full md:w-auto">Like</button>
       </div>
-    </>
+    </div>
   );
 };
 
